4786e74c90a6693b27bf27c71eb24c11c5d40a8f
Sawan Singh <sawansingh5216977@gmail.com>
2022-04-06 22:53:36 +0530

    created TeamPage & its component

---
+++ src/components/MatchDetailCard.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+
+export const MatchDetailCard = () => {
+  return (
+    <div className="MatchDetailCard">
+      <h1>Latest match</h1>
+      <h3>Match details</h3>
+    </div>
+  );
+};
---
+++ src/components/MatchSmallCard.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+export const MatchSmallCard = () => {
+  return (
+    <div className="MatchSmallCard">
+      <p>MatchSmallCard</p>
+    </div>
+  );
+};
