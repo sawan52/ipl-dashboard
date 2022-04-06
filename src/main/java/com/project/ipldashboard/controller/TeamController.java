b7be086e933fd665f9f538dfa482c12b21e334d9
Sawan Singh <sawansingh5216977@gmail.com>
2022-04-06 21:42:26 +0530

    created controller & repository for both Team & Match classes & show all the matches played by a particular team

---
+++ src/main/java/com/project/ipldashboard/controller/TeamController.java
@@ -0,0 +1,26 @@
+package com.project.ipldashboard.controller;
+
+import com.project.ipldashboard.model.Team;
+import com.project.ipldashboard.repository.MatchRepository;
+import com.project.ipldashboard.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+        Team team = teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
+        return team;
+    }
+}
--- src/main/java/com/project/ipldashboard/model/Team.java
+++ src/main/java/com/project/ipldashboard/model/Team.java
@@ -1,9 +1,7 @@
 package com.project.ipldashboard.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,6 +12,9 @@ public class Team {
     private String teamName;
     private long totalMatches;
     private long totalWins;
+
+    @Transient
+    private List<Match> matches;
 
     public Team() {
     }
@@ -53,6 +54,14 @@ public class Team {
 
     public void setTotalWins(long totalWins) {
         this.totalWins = totalWins;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
     }
 
     @Override
