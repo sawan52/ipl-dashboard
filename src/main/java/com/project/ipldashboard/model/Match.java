c05ee3b5ef5e5406ffff6a9184810a561741983d
Sawan Singh <sawansingh5216977@gmail.com>
2022-04-06 18:29:54 +0530

    created BatchConfig to read, transform & write data & inform using NotificationListener

--- src/main/java/com/project/ipldashboard/model/Match.java
+++ src/main/java/com/project/ipldashboard/model/Match.java
@@ -1,9 +1,13 @@
 package com.project.ipldashboard.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 public class Match {
 
+    @Id
     private long id;
     private String city;
     private LocalDate date;
