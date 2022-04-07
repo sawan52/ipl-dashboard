d8dad690c059104df42fd95cd91741261123e578
Sawan Singh <sawansingh5216977@gmail.com>
2022-04-07 13:23:45 +0530

    factorized data from api & used route feature to get other teams details as well

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,28 +1,32 @@
 import { React, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
 
   useEffect(() => {
     const fetchMatches = async () => {
-      const response = await fetch(
-        "http://localhost:8080/team/Rising Pune Supergiants"
-      );
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
       const data = await response.json();
       console.log(data);
       setTeam(data);
     };
     fetchMatches();
-  }, []);
+  }, [teamName]);
+
+  if (!team || !team.teamName) {
+    return <h1>Team not found</h1>;
+  }
 
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]} />
+      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
       {team.matches.slice(1).map((match) => (
-        <MatchSmallCard match={match} key={match.id} />
+        <MatchSmallCard teamName={team.teamName} match={match} key={match.id} />
       ))}
     </div>
   );
